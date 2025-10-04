b73a90d9519bafc385847f3c8bfccb583aeb37bc
KELVIN-WAKIARA <kelvinwakiara6@gmail.com>
2025-10-04 17:56:05 +0300

    future age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
-Current_age=32
-Year_born=2025-Current_age
+Current_age=30
+Year_born=2023-Current_age
 Future_year=2050
 Future_age=Future_year-Year_born
 print("In",Future_year,",you will be",Future_age,"years old.")
