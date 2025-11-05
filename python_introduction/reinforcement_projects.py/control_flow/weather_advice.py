3ef5137b454a5740d77597a5b4f6e5fad920a3ca
KELVIN-WAKIARA <kelvinwakiara6@gmail.com>
2025-11-05 07:33:08 +0300

    initial commit

---
+++ python_introduction/reinforcement_projects.py/control_flow/weather_advice.py
@@ -0,0 +1,9 @@
+prompt=input("Enter the weather condition (sunny, rainy,cold): ")
+if prompt == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif prompt == "rainy":
+    print("Dont forget your umbrella and raincoat.")
+elif prompt == "cold":
+    print("Make sure t-shirt wear a warm coat and a scarf.")
+else:
+    print("Sorry ,i don't have reccomendations for that weather.")
